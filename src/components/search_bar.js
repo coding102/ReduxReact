27db598ac5c1ad017744dd79c06d7b9364ceb191
Marck <mercercountynj@gmail.com>
2016-04-18 20:44:06 -0400

    initialize state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 //functional component to class based component gives additional functionality
 class SearchBar extends Component {
+     // class based state initialization
+     constructor(props) {
+          super(props);
+          
+          this.state = { term: '' };
+     }
      render () {
           //es6 in place of code below
           return <input onChange={(event) => console.log(event.target.value)} />;
