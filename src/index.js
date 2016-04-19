5b2816b6ff3b69a4fca03290c2af40ad990cdbe9
Marck <mercercountynj@gmail.com>
2016-04-19 15:21:06 -0400

    youtube api, videolist component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar.js';
 
+import VideoList from './components/video_list';
+// class based components are used when components need to be aware of state
+// function based components are used when taking in info and producing jsx
 const API_KEY = 'AIzaSyAfloA_6cKK0Yf6JgGkj8gx2kG0Z0sELk0';
-// create a new component 
-//const declares a final "variable" that is never going to be reassigned
-const App = () => {
-     return (
-          <div>
-               <SearchBar />
-          </div>
-     );
+
+// const declares a final "variable" that is never going to be reassigned
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        //YTSearch({key: API_KEY, term: 'surfboards'}, function(videos) {
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            //this.setState({ videos: videos }); below works only if key and prop are the same
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 
