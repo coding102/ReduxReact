b7864a9502706d73756bd86df275be5e221c40bd
Marck <mercercountynj@gmail.com>
2016-04-22 13:44:10 -0400

    page styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,10 +13,10 @@ class SearchBar extends Component {
           // es6 in place of code below
           // to change state use setState NEVER use just state
           return (
-               <div>
+               <div className="search-bar">
                     <input 
                          value={this.state.term}
-                         onChange={(event) => this.setState({ term: event.target.value })} />;
+                         onChange={(event) => this.setState({ term: event.target.value })} />
                </div>
           );
      }
