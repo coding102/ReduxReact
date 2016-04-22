6cbe192f5d04c5a81e848c8a47d6ea62d4a6364e
Marck <mercercountynj@gmail.com>
2016-04-21 23:40:46 -0400

    adding selection function to select videos

--- src/index.js
+++ src/index.js
@@ -13,12 +13,18 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         //YTSearch({key: API_KEY, term: 'surfboards'}, function(videos) {
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
             //this.setState({ videos: videos }); below works only if key and prop are the same
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -26,8 +32,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos} />
             </div>
         );
     }
